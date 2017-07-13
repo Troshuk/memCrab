c4d8f9937aeb673422f547f19b945bb5ac9012b8
Denis <Troshuk@gmail.com>
2017-07-14 00:49:27 +0300

    life without global

--- index.php
+++ index.php
@@ -2,8 +2,28 @@
 // merge, rebase, ferch, pull request, git flow, 
 // усе протестити, з 2-ма користувачами, різні варіанти і т.д. ...
 
-$safeMyParam = function(){
-	return 'no changes this text';
+function safeMyParam($name){
+	$name = "$name - changeName";
+	echo $name."\n";
+	// NAME = '123'; // Parse error
+}
+function safeMyClassParam($object){
+	$object->name = "$object->name - changeName";
+	echo $object->name."\n";
+}
+Class Aparat {
+	public $name = "nameAparat";
 }
 
-echo safeMyParam();
+$name = "Piter";
+$aparat = new Aparat();
+
+#1
+define('NAME', "Piter");
+#2
+safeMyParam($name);
+echo $name."\n";
+#3
+safeMyClassParam(clone $aparat);
+// safeMyClassParam($aparat); // передає силку на обєкт
+echo $aparat->name."\n";
