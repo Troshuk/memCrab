cded4f75bcf109debe5afd5e852e10e62d48f691
Denis <Troshuk@gmail.com>
2017-07-14 00:58:22 +0300

    edit task #1

--- index.php
+++ index.php
@@ -2,28 +2,31 @@
 // merge, rebase, ferch, pull request, git flow, 
 // усе протестити, з 2-ма користувачами, різні варіанти і т.д. ...
 
+Class Aparat {
+	public $name = "nameAparat";
+}
 function safeMyParam($name){
 	$name = "$name - changeName";
 	echo $name."\n";
 	// NAME = '123'; // Parse error
+	echo MYCONST."\n";
 }
 function safeMyClassParam($object){
 	$object->name = "$object->name - changeName";
 	echo $object->name."\n";
-}
-Class Aparat {
-	public $name = "nameAparat";
 }
 
 $name = "Piter";
 $aparat = new Aparat();
 
 #1
-define('NAME', "Piter");
+const MYCONST = 'You can\'t update this const';
 #2
+define('NAME', "Piter");
+#3
 safeMyParam($name);
 echo $name."\n";
-#3
+#4
 safeMyClassParam(clone $aparat);
 // safeMyClassParam($aparat); // передає силку на обєкт
 echo $aparat->name."\n";
