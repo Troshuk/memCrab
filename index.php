1e8c57f9d5a879ff1e21e37a638ceb7996715023
Denis <Troshuk@gmail.com>
2017-07-13 23:39:39 +0300

    create repository from memCrab

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+// merge, rebase, ferch, pull request, git flow, 
+// усе протестити, з 2-ма користувачами, різні варіанти і т.д. ...
+
+$safeMyParam = function(){
+	return 'no changes this text';
+}
+
+echo safeMyParam();
