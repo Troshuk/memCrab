12e2ec5cdc5991f10dcc9daba6690eb4f1d7a160
Denis <Troshuk@gmail.com>
2017-07-14 23:44:14 +0300

    edit #1

--- index.php
+++ index.php
@@ -2,31 +2,85 @@
 // merge, rebase, ferch, pull request, git flow, 
 // усе протестити, з 2-ма користувачами, різні варіанти і т.д. ...
 
-Class Aparat {
-	public $name = "nameAparat";
-}
-function safeMyParam($name){
-	$name = "$name - changeName";
-	echo $name."\n";
-	// NAME = '123'; // Parse error
-	echo MYCONST."\n";
-}
-function safeMyClassParam($object){
-	$object->name = "$object->name - changeName";
-	echo $object->name."\n";
+class MusicClass {
+	public static $musicName = "Nuvoile Biench";
 }
 
-$name = "Piter";
-$aparat = new Aparat();
+echo MusicClass::$musicName."\n";
 
-#1
-const MYCONST = 'You can\'t update this const';
-#2
-define('NAME', "Piter");
-#3
-safeMyParam($name);
-echo $name."\n";
-#4
-safeMyClassParam(clone $aparat);
-// safeMyClassParam($aparat); // передає силку на обєкт
-echo $aparat->name."\n";
+class Singelton {
+
+	protected static $instance = "singelton";
+
+	protected function __construct(){
+		
+	}
+
+	protected function __clone(){
+
+	}
+
+	public static function getInstance(){
+		if (!isset(static::$instance)) self::$instance = new Singelton();
+		return static::$instance;
+	}
+}
+
+echo Singelton::getInstance()."\n";
+
+class PeopleDB
+{
+    protected $database;
+    protected $title;
+    protected $first_name;
+    protected $last_name;
+     
+    public function __construct(mysqli $DB, $last_name)
+    {
+        $this->database = $DB;
+        if (strlen($last_name) == 0) {
+            throw new Exception('Last name required');
+        }
+        $this->last_name = $last_name;
+    }
+     
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    public function setFirstName($first_name)
+    {
+        $this->first_name = $first_name;
+    }
+
+    public function setLastName($last_name)
+    {
+        $this->last_name = $last_name;
+    }
+     
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function getFirstName()
+    {
+        return $this->first_name;
+    }
+
+    public function getLastName()
+    {
+        return $this->last_name;
+    }
+
+}
+$DB = new mysqli('localhost', 'root', '');
+$man = new PeopleDB($DB, 'Smith');
+$man->setTitle('Mr');
+$man->setFirstName('Mister');
+$man->setLastName('Mister');
+
+echo $man->getTitle()."\n";
+echo $man->getFirstName()."\n";
+echo $man->getLastName()."\n";
