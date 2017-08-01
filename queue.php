ff13832cbc5fe112f88c91306e29de63b0703e63
Troshuk <Troshuk@gmail.com>
2017-08-01 15:35:37 +0300

    start mq

--- queue.php
+++ queue.php
@@ -1,10 +1,24 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
-use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Connection\AMQPConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
+$connection_params = array(
+  'host' => 'localhost',
+  'port' => 5672,
+  'vhost' => '/',
+  'login' => 'guest',
+  'password' => 'guest'
+);
+
+
+$connection = new AMQPConnection($connection_params);
+$connection->connect();
+
+
+/*
 $connection = new AMQPStreamConnection('localhost', 15672, 'guest', 'guest');
-$channel = $connection->channel();
+$channel = $connection->channel();*/
 // $channel = $connection->chanel();
 // $connection->connect();
 
