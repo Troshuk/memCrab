1d93bc9492d304fe650759b888124485273ddaf7
Denis <Troshuk@gmail.com>
2017-08-07 09:54:44 +0300

    gettext is complete

--- json_array.php
+++ json_array.php
@@ -1,16 +1,34 @@
 <?php
 
 $memory = memory_get_usage();
+echo round( ( memory_get_usage() ) / 1024 / 1024, 5 ) . " MB (до) \n";
+echo " Create array \n";
 $z = [];
 for ($i=0; $i < 1000000; $i++) { 
+	$z[$i] = "$i$i$i$i$i$i$i$i$i$i$i$i$i$i$i$i$i$i$i$i$i$i$i$i$i$i$i$i$i$i$i$i";
+}
+echo round( ( memory_get_usage() ) / 1024 / 1024, 5 ) . " MB (після) \n";
+// echo round( ( memory_get_usage() - $memory ) / 1024 / 1024, 5 ) . " MB \n";
+
+$memory = memory_get_usage();
+echo round( ( memory_get_usage() ) / 1024 / 1024, 5 ) . " MB (до) \n";
+echo " json_encode \n";
+$z = json_encode( $z, JSON_UNESCAPED_UNICODE );
+echo round( ( memory_get_usage() ) / 1024 / 1024, 5 ) . " MB (після) \n";
+// echo round( ( memory_get_usage() - $memory ) / 1024 / 1024, 5 ) . " MB \n";
+
+$memory = memory_get_usage();
+echo round( ( memory_get_usage() ) / 1024 / 1024, 5 ) . " MB (до) \n";
+echo " json_decode \n";
+$z = json_decode($z, true);
+echo round( ( memory_get_usage() ) / 1024 / 1024, 5 ) . " MB (після) \n";
+// echo round( ( memory_get_usage() - $memory ) / 1024 / 1024, 5 ) . " MB \n";
+
+
+echo round( ( memory_get_usage() ) / 1024 / 1024, 5 ) . " MB (до) \n";
+$z = [];
+echo " Update array \n";
+for ($i=0; $i < 1000; $i++) { 
 	$z[] = "$i$i$i$i$i$i$i$i$i$i$i$i$i$i$i$i$i$i$i$i$i$i$i$i$i$i$i$i$i$i$i$i";
 }
-echo round( ( memory_get_usage() - $memory ) / 1024 / 1024, 5 ) . " MB \n";
-
-$memory = memory_get_usage();
-$json = json_encode( $z, JSON_UNESCAPED_UNICODE );
-echo round( ( memory_get_usage() - $memory ) / 1024 / 1024, 5 ) . " MB \n";
-
-$memory = memory_get_usage();
-$json2 = json_decode($json, true);
-echo round( ( memory_get_usage() - $memory ) / 1024 / 1024, 5 ) . " MB \n";
+echo round( ( memory_get_usage() ) / 1024 / 1024, 5 ) . " MB (після) \n";
--- redis.php
+++ redis.php
@@ -82,17 +82,22 @@ $redis = new Predis\Client(array(
 
 // echo $value . "\n";
 
-$start = microtime(true);
 
 // for ($i = 0; $i < 1000; $i++) {
 // 	$key = "product-$i";
 // 	$redis->sadd($key, ['water-' . $i, $i + $i * 2, $i * 2]);
 // }
 
-for ($i = 0; $i < 1000; $i++) {
-	$key = "product-$i";
-	$value = $redis->smembers($key);
-	// print_r($value);
-}
+// for ($i = 0; $i < 1000; $i++) {
+// 	$key = "product-$i";
+// 	$value = $redis->smembers($key);
+// 	// print_r($value);
+// }
 
+// $value = $redis->smembers('product-1');
+// print_r($value);
+
+$start = microtime(true);
+$value = $redis->keys('*');
+print_r($value);
 echo round(microtime(true) - $start, 4) . "c. \n";
